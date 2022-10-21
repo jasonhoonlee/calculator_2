a68ff403461fd94a4969b19b28c1a068b17a9eb6
Hoon Lee <jasonhoonlee@gmail.com>
2022-10-21 11:25:47 -0700

    Create and implement 'build current operand' feature

--- script.js
+++ script.js
@@ -2,10 +2,34 @@
 
 
 const calculator = {
-  currentScreen: null,
+  currentScreen: '',
   firstOperand: '',
   secondOperand: '',
   currentOperand: 'first',
 }
 
 
+
+
+function buildCurrentOperand(e) {
+  const clickedButton = e.target;
+  if (clickedButton.classList.contains('number')) {
+    if (calculator.currentOperand === 'first') {
+      calculator.firstOperand += clickedButton.textContent;
+    } else {
+      calculator.secondOperand += clickedButton.textContent;
+    }
+  }
+}
+
+
+
+
+
+(function() {
+  //query selectors
+  const buttons = document.querySelectorAll('.btn');
+
+  //event listeners
+  buttons.forEach(button => button.addEventListener('click', buildCurrentOperand));
+})()
