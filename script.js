bef36049cc4095dc643818f78c8da2389cc01b74
Hoon Lee <jasonhoonlee@gmail.com>
2022-11-07 11:15:02 -0800

    Delete all functionality in script file and start over

--- script.js
+++ script.js
@@ -6,30 +6,14 @@ const calculator = {
   firstOperand: '',
   secondOperand: '',
   currentOperand: 'first',
+  currentOperator: null,
 }
-
-
-
-
-function buildCurrentOperand(e) {
-  const clickedButton = e.target;
-  if (clickedButton.classList.contains('number')) {
-    if (calculator.currentOperand === 'first') {
-      calculator.firstOperand += clickedButton.textContent;
-    } else {
-      calculator.secondOperand += clickedButton.textContent;
-    }
-  }
-}
-
 
 
 
 
 (function() {
-  //query selectors
-  const buttons = document.querySelectorAll('.btn');
 
-  //event listeners
-  buttons.forEach(button => button.addEventListener('click', buildCurrentOperand));
 })()
+
+
