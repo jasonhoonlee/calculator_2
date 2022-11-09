d4919e301ba54864a865c3ae10bd02979c30887a
Hoon Lee <jasonhoonlee@gmail.com>
2022-11-08 19:12:38 -0800

    Implement 'process sign button' feature
    -Toggle between positive and negative version of the current operand

--- script.js
+++ script.js
@@ -33,7 +33,6 @@ function getButtonIdentity(clickedButton) {
     return {type, value};
   }
   return {type}
-
 }
 
 
@@ -41,6 +40,7 @@ function updateCalculatorState(buttonInfo) {
   //NUMBER
   if (buttonInfo.type === 'number') processNumberButton(buttonInfo.value);
   //NUMBER SIGN
+  if (buttonInfo.type === 'sign') processSignButton();
   //DECIMAL
   //PERCENTAGE
   //1/X
@@ -58,6 +58,22 @@ function processNumberButton(value) {
   if (calculator.currentOperand === 'first') calculator.firstOperand += value;
   else calculator.secondOperand += value;
   console.log(calculator)
+}
+
+function processSignButton() {
+  if (calculator.currentOperand === 'first') {
+    if (calculator.firstOperand.includes('-')) {
+      calculator.firstOperand = calculator.firstOperand.slice(2, calculator.firstOperand.length-1);
+    } else {
+      calculator.firstOperand = `(-${calculator.firstOperand})`;
+    }
+  } else {
+    if (calculator.secondOperand.includes('-')) {
+      calculator.secondOperand = calculator.secondOperand.slice(2, calculator.secondOperand.length-1);
+    } else {
+      calculator.secondOperand = `(-${calculator.secondOperand})`;
+    }
+  }
 }
 
 
