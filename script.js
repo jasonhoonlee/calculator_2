9032b20047e13e47be508118620257433e84daf6
Hoon Lee <jasonhoonlee@gmail.com>
2022-11-29 05:52:20 -0800

    Add to 'process divide button' feature
    -Make sure current operand value is a number value before processing button.

--- script.js
+++ script.js
@@ -233,6 +233,11 @@ function processSubtractButton() {
 }
 
 function processDivideButton() {
+  let currentOperandValue;
+  if (calculator.currentOperand === 'first') currentOperandValue = calculator.firstOperand;
+  if (calculator.currentOperand === 'second') currentOperandValue = calculator.secondOperand;
+  if (!isNumberValue(currentOperandValue)) return;
+
   if (calculator.firstOperand && calculator.secondOperand) {
     calculator.firstOperand = String(evaluate());
     calculator.secondOperand = '';
